d42dcd47d08801c405326f606ab5359fdb8e6883
김현민 <gusals072@gmail.com>
2024-10-29 16:31:32 +0900

    프론트라인 기반 추가

--- demo/src/main/java/com/hairtalk/demo/Member.java
+++
@@ -1,9 +0,0 @@
-package com.hairtalk.demo;
-
-import lombok.extern.slf4j.Slf4j;
-
-
-public class Member {
-    String name;
-    int age;
-}
--- demo/src/test/java/com/hairtalk/demo/Test.java
+++
@@ -1,9 +0,0 @@
-package com.hairtalk.demo;
-
-import org.springframework.stereotype.Component;
-
-@Component
-
-public class Test {
-
-}
