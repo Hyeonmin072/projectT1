223cf64c7f3be94ef6ec6a6c4acce6b867b8eb01
김현민 <gusals072@gmail.com>
2024-10-29 19:15:44 +0900

    전체적 분위기 수정

--- GameSnap/frontend/src/component/Hero.js
+++ GameSnap/frontend/src/component/Hero.js
@@ -3,16 +3,13 @@ import React from 'react';
 
 function Hero() {
   return (
-    <section className="h-screen bg-blue-600 text-white flex flex-col justify-center items-center text-center">
-      <h1 className="text-4xl font-bold mb-4">Welcome to HairTalk</h1>
-      <p className="text-lg mb-8">I'm a Full-Stack Developer</p>
-      <a href="#projects" className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white">
-        회원로그인
+    <section className="h-screen bg-white-600 text-white flex flex-col justify-center items-center text-center">
+      <h1 className="text-4xl font-bold mb-4 text-black">Welcome to GameSnap</h1>
+      <p className="text-lg mb-8 text-black">어서오세요, GameSnap 입니다.</p>
+      <a href="#projects" className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white hover ">
+        회원 로그인
       </a>
       <br></br>
-      <a href="#projects" className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white">
-        디자이너 로그인
-        </a>
     </section>
   );
 }
--- GameSnap/frontend/src/component/Navbar.js
+++ GameSnap/frontend/src/component/Navbar.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-lg p-4 flex justify-between">
-      <div className="text-2xl font-bold text-blue-600">MyPortfolio</div>
+    <nav className="bg-black shadow-lg p-4 flex justify-between">
+      <div className="text-2xl font-bold text-white">GameSnap</div>
       <ul className="flex space-x-4">
-        <li><a href="#about" className="hover:text-blue-600">About</a></li>
-        <li><a href="#projects" className="hover:text-blue-600">Projects</a></li>
-        <li><a href="#contact" className="hover:text-blue-600">Contact</a></li>
+        <li><a href="#about" className="hover:text-green-600 text-white font-bold">About</a></li>
+        <li><a href="#projects" className="hover:text-green-600 text-white font-bold">Projects</a></li>
+        <li><a href="#contact" className="hover:text-green-600 text-white font-bold">Contact</a></li>
       </ul>
     </nav>
   );
