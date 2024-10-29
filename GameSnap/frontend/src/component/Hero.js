1a3b611f04dae9f0598ea279ed204a536e3204c4
김현민 <gusals072@gmail.com>
2024-10-29 19:26:47 +0900

    상단바 조정

--- GameSnap/frontend/src/component/Hero.js
+++ GameSnap/frontend/src/component/Hero.js
@@ -6,9 +6,7 @@ function Hero() {
     <section className="h-screen bg-white-600 text-white flex flex-col justify-center items-center text-center">
       <h1 className="text-4xl font-bold mb-4 text-black">Welcome to GameSnap</h1>
       <p className="text-lg mb-8 text-black">어서오세요, GameSnap 입니다.</p>
-      <a href="#projects" className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white hover ">
-        회원 로그인
-      </a>
+
       <br></br>
     </section>
   );
--- GameSnap/frontend/src/component/Navbar.js
+++ GameSnap/frontend/src/component/Navbar.js
@@ -5,10 +5,17 @@ function Navbar() {
   return (
     <nav className="bg-black shadow-lg p-4 flex justify-between">
       <div className="text-2xl font-bold text-white">GameSnap</div>
+
+
       <ul className="flex space-x-4">
-        <li><a href="#about" className="hover:text-green-600 text-white font-bold">About</a></li>
-        <li><a href="#projects" className="hover:text-green-600 text-white font-bold">Projects</a></li>
-        <li><a href="#contact" className="hover:text-green-600 text-white font-bold">Contact</a></li>
+        <div className="flex justify-end">
+          <a href="#projects" className="px-6 py-1 bg-white text-black-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white">
+            회원 로그인
+          </a>
+        </div>
+        <li><a href="#about" className="flex flex-col items-end space-y-2 hover:text-green-600 text-white font-bold ">About</a></li>
+        <li><a href="#projects" className="flex flex-col items-end space-y-2 hover:text-green-600 text-white font-bold">Projects</a></li>
+        <li><a href="#contact" className="flex flex-col items-end space-y-2 hover:text-green-600 text-white font-bold">Contact</a></li>
       </ul>
     </nav>
   );
