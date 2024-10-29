622912bd7af078713aed6d962c2cde9b2b324630
김현민 <gusals072@gmail.com>
2024-10-29 19:29:15 +0900

    로그인 버튼 호버링 수정

--- GameSnap/frontend/src/component/Navbar.js
+++ GameSnap/frontend/src/component/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
 
       <ul className="flex space-x-4">
         <div className="flex justify-end">
-          <a href="#projects" className="px-6 py-1 bg-white text-black-600 font-semibold rounded-md hover:bg-blue-700 hover:text-white">
+          <a href="#projects" className="px-6 py-1 bg-white text-black-600 font-semibold rounded-md hover:bg-green-700 hover:text-white">
             회원 로그인
           </a>
         </div>
