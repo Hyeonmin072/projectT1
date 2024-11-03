7a6ee17c86abd4b1a2938d743d36b9a77ea0fcc6
김현민 <gusals072@gmail.com>
2024-11-03 18:47:08 +0900

    Squashed commit of the following:
    commit 5c8293600909c8807ee9053d673daf59aa300663
    Author: 김현민 <gusals072@gmail.com>
    Date:   Sun Nov 3 18:46:14 2024 +0900
    
        Squashed commit of the following:
    
        commit 0d22386706688a7fb878d277afea7d253a7501f1
        Author: rlawjggns <rlawjggns12@gmail.com>
        Date:   Sun Nov 3 18:44:51 2024 +0900
    
            로그인 및 회원가입 정상화
    
    commit 9bce20811c5f2aae88e46011c4e08302c6c9b334
    Author: 김현민 <gusals072@gmail.com>
    Date:   Sun Nov 3 18:44:58 2024 +0900
    
        sd
    
    commit 1ec4467f2666b434b80289985c5f77b1fddd6213
    Author: 김현민 <gusals072@gmail.com>
    Date:   Sun Nov 3 18:33:01 2024 +0900
    
        sd

--- GameSnap/frontend/src/component/PageContainer.js
+++ GameSnap/frontend/src/component/PageContainer.js
@@ -4,45 +4,40 @@ import MainPage from './MainPage';
 import UserPage from './UserPage';
 
 const PageContainer = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-
-  const handleLoginSuccess = (data) => {
-    setIsTransitioning(true);
-    // 로그인 성공 후 애니메이션 처리
-    setTimeout(() => {
-      setIsAuthenticated(true);
-    }, 300); // 애니메이션 duration과 일치
-  };
-
-  return (
-    <div className="relative overflow-hidden w-full h-screen">
-      {/* 메인 페이지와 유저 페이지 컨테이너 */}
-      <div
-        className={`flex transition-transform duration-300 ease-in-out w-[200%] h-full
-          ${isTransitioning ? '-translate-x-1/2' : 'translate-x-0'}`}
-      >
-        {/* 메인 페이지 */}
-        <div className="w-full h-full flex-shrink-0">
-          <MainPage />
-          {!isAuthenticated && (
-            <div className="fixed inset-0 z-50">
-              <LoginForm
-                onLoginSuccess={handleLoginSuccess}
-                onClose={() => {}}
-                onRegisterClick={() => {}}
-              />
-            </div>
-          )}
-        </div>
-
-        {/* 유저 페이지 */}
-        <div className="w-full h-full flex-shrink-0">
-          <UserPage />
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+  
+    const handleLoginSuccess = (data) => {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setIsAuthenticated(true);
+      }, 300);
+    };
+  
+    return (
+      <div className="relative w-full h-screen"> {/* overflow-hidden 제거 */}
+        <div
+          className={`flex transition-transform duration-300 ease-in-out w-[200%] h-full
+            ${isTransitioning ? '-translate-x-1/2' : 'translate-x-0'}`}
+        >
+          <div className="w-full h-full flex-shrink-0 bg-gray-100"> {/* bg-gray-100 추가 */}
+            <MainPage />
+            {!isAuthenticated && (
+              <div className="fixed inset-0 z-50">
+                <LoginForm
+                  onLoginSuccess={handleLoginSuccess}
+                  onClose={() => {}}
+                  onRegisterClick={() => {}}
+                />
+              </div>
+            )}
+          </div>
+          <div className="w-full h-full flex-shrink-0">
+            <UserPage />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default PageContainer;
