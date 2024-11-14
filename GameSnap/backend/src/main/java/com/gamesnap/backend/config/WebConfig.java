fff304c9bface01deb107a917245f3d16ac7a78d
HYEONMIN <135511648+Hyeonmin072@users.noreply.github.com>
2024-11-14 20:33:39 +0900

    ㅇㄹ

--- GameSnap/backend/src/main/java/com/gamesnap/backend/config/WebConfig.java
+++ GameSnap/backend/src/main/java/com/gamesnap/backend/config/WebConfig.java
@@ -1,32 +1,19 @@
 package com.gamesnap.backend.config;
 
-<<<<<<< HEAD
-=======
-import org.springframework.beans.factory.annotation.Value;
->>>>>>> 8c9a466f143d19961e0ca0ae9a935bd493a1935b
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${FRONTEND_URLS}")
-    private String frontendUrls;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-<<<<<<< HEAD
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-=======
-                .allowedOrigins(frontendUrls.split(","))
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
->>>>>>> 8c9a466f143d19961e0ca0ae9a935bd493a1935b
     }
 }
