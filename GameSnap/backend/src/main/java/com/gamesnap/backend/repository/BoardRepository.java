d4220c83721a8e6b6c2f61ca2b627386ae8193f0
김현민 <gusals072@gmail.com>
2024-11-18 12:02:03 +0900

    게시판 내림차

--- GameSnap/backend/src/main/java/com/gamesnap/backend/repository/BoardRepository.java
+++ GameSnap/backend/src/main/java/com/gamesnap/backend/repository/BoardRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-    Page<Board> findByGame(Game game, Pageable pageable);
+    Page<Board> findByGameOrderByCreateDateDesc(Game game, Pageable pageable);
+
     Optional<Board> findByTitle(String title);
 }
