96c08c7b1ae25bbfb9cda49c37249075fc4111c9
김현민 <gusals072@gmail.com>
2024-11-22 13:51:53 +0900

    sd

--- GameSnap/frontend/src/hooks/useUpdateContent.js
+++ GameSnap/frontend/src/hooks/useUpdateContent.js
@@ -1,9 +1,12 @@
 // hooks/useUpdateContent.js
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
+
 
 export const useUpdateContent = () => {
   const queryClient = useQueryClient();
+  const { userData } = useAuth();
 
   return useMutation({
     mutationKey: ['updateContent'],
@@ -11,7 +14,9 @@ export const useUpdateContent = () => {
       try {
         const token = localStorage.getItem('token');
         const response = await axios.post('http://localhost:1111//Profile/content', 
-          { content }, 
+          { content,
+            userId: userData.id,
+          }, 
           {
             headers: {
               'Content-Type': 'application/json',
